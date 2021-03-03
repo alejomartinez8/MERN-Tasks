dea14a17552f16e9b6b7d3d0bbebae63fca9cf23
Alejandro Martinez <alejomartinez8@gmail.com>
2021-03-03 12:10:01 -0500

    Add Login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Login, NewAccount, Projects } from "./components";
+
 function App() {
-	return <h1>MERN Tasks</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/new-account" component={NewAccount} />
+        <Route exact path="/projects" component={Projects} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
