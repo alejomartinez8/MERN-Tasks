9fb6f0461d33beaaf403256d941a39c743be7b89
Alejandro Martinez <alejomartinez8@gmail.com>
2021-03-13 16:08:36 -0500

    Add New Project form

--- client/src/components/projects/NewProjectForm.jsx
+++ client/src/components/projects/NewProjectForm.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewProjectForm = () => {
+  const [project, setProject] = useState({
+    name: "",
+  });
+
+  const { name } = project;
+
+  const handleOnChange = (e) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <button type="button" className="btn btn-block btn-primario">
         New Project
       </button>
-      <form action="" className="formulario-nuevo-proyecto">
+      <form onSubmit={handleOnSubmit} className="formulario-nuevo-proyecto">
         <input
           type="text"
           className="input-text"
           placeholder="Project Name"
           name="name"
+          onChange={handleOnChange}
+          value={name}
         />
         <input
           type="submit"
